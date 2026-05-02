415b38750ec34445ac2eec17fda21d231b5ad043
Anuchit Chalothorn <anoochit@gmail.com>
2026-05-02 14:10:25 +0700

    chore: remove redundant git-sync scripts

--- git-sync.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set MESSAGE=%1
-if "%MESSAGE%"=="" set MESSAGE=Update codebase
-
-git add .
-git commit -m "%MESSAGE%"
-git push
--- git-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Default commit message
-MESSAGE=${1:-"Update codebase"}
-
-git add .
-git commit -m "$MESSAGE"
-git push
